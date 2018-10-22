d3b9947482618e71398a052a51681a5e1a33887d
Anna Molly <akhsunna4@gmail.com>
2018-10-22 12:06:24 +0300

    Small refactoring
    - remove `RailsAdminShrine` module as it is not used and everything works without its declaration
    
    - no need in `||=` if @thumb_method is defined
    - _attacher is always present, so no need to check it
    - versions should be symbols
    
    - inside `resource_url` now is used `value`
    
    - refactor factory

--- lib/rails_admin_shrine.rb
+++ lib/rails_admin_shrine.rb
@@ -1,11 +1,7 @@
 require "rails_admin_shrine/engine"
-
-module RailsAdminShrine
-end
-
-require 'rails_admin/config/fields'
-require 'rails_admin/config/fields/base'
-require 'rails_admin/config/fields/types/file_upload'
+require "rails_admin/config/fields"
+require "rails_admin/config/fields/base"
+require "rails_admin/config/fields/types/file_upload"
 
 module RailsAdmin
   module Config
@@ -16,15 +12,13 @@ module RailsAdmin
 
           register_instance_option :thumb_method do
             unless defined? @thumb_method
-              @thumb_method ||= begin              
-                attacher = bindings[:object].try("#{name}_attacher".to_sym)              
-                if attacher
-                  versions = attacher.shrine_class.version_names 
-                  versions.detect{|v| v.in?([:thumb, :thumbnail, 'thumb', 'thumbnail']) } || versions.first.to_s
-                end  
+              @thumb_method = begin
+                attacher = bindings[:object].public_send("#{name}_attacher".to_sym)
+                versions = attacher.shrine_class.version_names.map { |v| v.to_sym }
+                versions.detect { |v| v.in?(%i[thumb thumbnail]) } || versions.first
               end
             end
-            @thumb_method  
+            @thumb_method
           end
 
           register_instance_option :delete_method do
@@ -36,12 +30,13 @@ module RailsAdmin
           end
 
           def resource_url(thumb = false)
-            return nil unless (uploader = bindings[:object].send(name)).present?                        
-            if uploader.is_a? Hash # has versions
-              thumb.present? ? uploader[thumb].url : uploader[uploader.keys.first].url
+            return nil unless value
+
+            if value.is_a?(Hash)
+              value[thumb || value.keys.first].url
             else
-              uploader.url  
-            end  
+              value.url
+            end
           end
         end
       end
@@ -50,24 +45,25 @@ module RailsAdmin
 end
 
 RailsAdmin::Config::Fields.register_factory do |parent, properties, fields|
-  model = parent.abstract_model.model  
-  if defined?(::Shrine) && (attachment_names = (model).ancestors.select{|m| m.is_a? Shrine::Attachment}.map{|a| a.instance_variable_get("@name")}).any? && (attachment_name = attachment_names.detect{|a| a == properties.name.to_s.chomp('_data').to_sym})
-    columns = [attachment_name, "#{attachment_name}_data".to_sym]
-    field = RailsAdmin::Config::Fields::Types.load(:shrine).new(parent, attachment_name, properties)
-    fields << field
-    children_fields = []
-    columns.each do |children_column_name|
-      next unless child_properties = parent.abstract_model.properties.detect { |p| p.name.to_s == children_column_name.to_s }
-      children_field = fields.detect { |f| f.name == children_column_name } || RailsAdmin::Config::Fields.default_factory.call(parent, child_properties, fields)
-      children_field.hide unless field == children_field
-      children_field.filterable(false) unless field == children_field
-      children_fields << children_field.name
-    end
-    field.children_fields(children_fields)
-    true
-  else
-    false
-  end
+  next false unless defined?(::Shrine)
+
+  attachment_names = (parent.abstract_model.model).ancestors.select { |m| m.is_a?(Shrine::Attachment) }.map{ |a| a.instance_variable_get("@name") }
+  next false if attachment_names.blank?
+
+  attachment_name = attachment_names.detect { |a| a == properties.name.to_s.chomp('_data').to_sym }
+  next false unless attachment_name
+
+  field = RailsAdmin::Config::Fields::Types.load(:shrine).new(parent, attachment_name, properties)
+  fields << field
+
+  data_field_name = "#{attachment_name}_data".to_sym
+  child_properties = parent.abstract_model.properties.detect { |p| p.name == data_field_name }
+  next true unless child_properties
+
+  children_field = fields.detect { |f| f.name == data_field_name } || RailsAdmin::Config::Fields.default_factory.call(parent, child_properties, fields)
+  children_field.hide unless field == children_field
+  children_field.filterable(false) unless field == children_field
+
+  field.children_fields([data_field_name])
+  true
 end
-
-
